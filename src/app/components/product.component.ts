fd6e2b22e9a4d5a7bbe17c3becffe3461a86c9d3
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-12-04 12:47:07 -0500

    adding styles

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,14 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
 
 import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnChanges{
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor(){
+
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('OnChanges')
+  }
 
   addCar() {
     this.productClicked.emit(this.product.id);
