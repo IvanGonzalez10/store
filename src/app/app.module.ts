a35a65951eb07c4185894162d4702ca2550a7ead
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-12-05 19:36:56 -0500

    doing directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ExponentialPipe } from './exponential.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe],
+  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe, HighlightDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
