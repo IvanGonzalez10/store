15f8c0a2aed57d6e8371aa46ef0390cce1da77ed
IvanGonzalez10 <ivan.c.gonzalez1@gmail.com>
2020-12-05 20:06:37 -0500

    changing images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,23 @@ import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ExponentialPipe } from './exponential.pipe';
 import { HighlightDirective } from './highlight.directive';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe, HighlightDirective],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CartComponent,
+    ExponentialPipe,
+    HighlightDirective,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
